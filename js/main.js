b70ad3f051a87f6e025204387b96e98100beb0e3
Tung <hiimtung123@gmail.com>
2020-11-09 07:48:12 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -15,11 +15,81 @@ function nextSlide(){
 	kichThuoc += 100;
 	if(kichThuoc>400) kichThuoc=0;
 	document.getElementById("chuyenSlide").style.marginLeft = '-' + kichThuoc + '%';
+	if(kichThuoc == 0){
+			document.getElementById("bottom_1").style.backgroundColor="#fff";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+		if(kichThuoc == 100){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#fff";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+		if(kichThuoc == 200){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#fff";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+		if(kichThuoc == 300){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#fff";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+		if(kichThuoc == 400){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#fff";
+		}
 }
 function prevSlide(){
 	kichThuoc -= 100;
 	if(kichThuoc<0) kichThuoc=400;
 	document.getElementById("chuyenSlide").style.marginLeft = '-' + kichThuoc + '%';
+	if(kichThuoc == 0){
+			document.getElementById("bottom_1").style.backgroundColor="#fff";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+	if(kichThuoc == 100){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#fff";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+		if(kichThuoc == 200){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#fff";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+		if(kichThuoc == 300){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#fff";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
+		}
+		if(kichThuoc == 400){
+			document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#fff";
+		}
 }
 setInterval(function(){
 	nextSlide();
@@ -27,60 +97,45 @@ setInterval(function(){
 function dieuHuong(){
 	kichThuoc = 0;
 	document.getElementById("chuyenSlide").style.marginLeft = '-' + kichThuoc + '%';
+	document.getElementById("bottom_1").style.backgroundColor="#fff";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
 }
 function dieuHuong2(){
 	kichThuoc = 100;
 	document.getElementById("chuyenSlide").style.marginLeft = '-' + kichThuoc + '%';
+	document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#fff";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
 }
 function dieuHuong3(){
 	kichThuoc = 200;
 	document.getElementById("chuyenSlide").style.marginLeft = '-' + kichThuoc + '%';
+	document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#fff";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
 }
 function dieuHuong4(){
 	kichThuoc = 300;
 	document.getElementById("chuyenSlide").style.marginLeft = '-' + kichThuoc + '%';
+	document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#fff";
+			document.getElementById("bottom_5").style.backgroundColor="#a5a5a5";
 }
 function dieuHuong5(){
 	kichThuoc = 400;
 	document.getElementById("chuyenSlide").style.marginLeft = '-' + kichThuoc + '%';
-}
-
-(function($) {
-	$(document).ready(function(){
-		if(kichThuoc == 0){
-			document.getElementById("bottom_1").style.backgroundColor="#fff";
-			document.getElementById("bottom_2").style.backgroundColor="none";
-			document.getElementById("bottom_3").style.backgroundColor="none";
-			document.getElementById("bottom_4").style.backgroundColor="none";
-			document.getElementById("bottom_5").style.backgroundColor="none";
-		}
-		if(kichThuoc == 100){
-			document.getElementById("bottom_1").style.backgroundColor="none";
-			document.getElementById("bottom_2").style.backgroundColor="#fff";
-			document.getElementById("bottom_3").style.backgroundColor="none";
-			document.getElementById("bottom_4").style.backgroundColor="none";
-			document.getElementById("bottom_5").style.backgroundColor="none";
-		}
-		if(kichThuoc == 200){
-			document.getElementById("bottom_1").style.backgroundColor="none";
-			document.getElementById("bottom_2").style.backgroundColor="none";
-			document.getElementById("bottom_3").style.backgroundColor="#fff";
-			document.getElementById("bottom_4").style.backgroundColor="none";
-			document.getElementById("bottom_5").style.backgroundColor="none";
-		}
-		if(kichThuoc == 300){
-			document.getElementById("bottom_1").style.backgroundColor="none";
-			document.getElementById("bottom_2").style.backgroundColor="none";
-			document.getElementById("bottom_3").style.backgroundColor="none";
-			document.getElementById("bottom_4").style.backgroundColor="#fff";
-			document.getElementById("bottom_5").style.backgroundColor="none";
-		}
-		if(kichThuoc == 400){
-			document.getElementById("bottom_1").style.backgroundColor="none";
-			document.getElementById("bottom_2").style.backgroundColor="none";
-			document.getElementById("bottom_3").style.backgroundColor="none";
-			document.getElementById("bottom_4").style.backgroundColor="none";
+	document.getElementById("bottom_1").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_2").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_3").style.backgroundColor="#a5a5a5";
+			document.getElementById("bottom_4").style.backgroundColor="#a5a5a5";
 			document.getElementById("bottom_5").style.backgroundColor="#fff";
-		}
-});
-})(jQuery);
+}
