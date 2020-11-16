8c506094b22fc5989be4d8565577ac482386b2e5
Tuan <vlhanoingucfga@gmail.com>
2020-11-16 21:19:45 +0700

    Thêm khuyến mại 2

--- js/js.js
+++ js/js.js
@@ -4,6 +4,9 @@ function doiboxmenu1() {
     document.getElementById("box-km-3").className = null;
     document.getElementById("box-km-4").className = null;
     document.getElementById("km-content-1").style = "display:block;";
+    document.getElementById("km-content-2").style = "display:none;";
+    document.getElementById("km-content-3").style = "display:none;";
+    document.getElementById("km-content-4").style = "display:none;";
 }
 
 function doiboxmenu2() {
@@ -11,6 +14,10 @@ function doiboxmenu2() {
     document.getElementById("box-km-2").className = "box-menu-active";
     document.getElementById("box-km-3").className = null;
     document.getElementById("box-km-4").className = null;
+    document.getElementById("km-content-1").style = "display:none;";
+    document.getElementById("km-content-2").style= "display:block;";
+    document.getElementById("km-content-3").style = "display:none;";
+    document.getElementById("km-content-4").style = "display:none;";
 }
 
 function doiboxmenu3() {
@@ -18,6 +25,10 @@ function doiboxmenu3() {
     document.getElementById("box-km-2").className = null;
     document.getElementById("box-km-3").className = "box-menu-active";
     document.getElementById("box-km-4").className = null;
+    document.getElementById("km-content-1").style = "display:none;";
+    document.getElementById("km-content-2").style= "display:none;";
+    document.getElementById("km-content-3").style = "display:block;";
+    document.getElementById("km-content-4").style = "display:none;";
 }
 
 function doiboxmenu4() {
@@ -25,4 +36,8 @@ function doiboxmenu4() {
     document.getElementById("box-km-2").className = null;
     document.getElementById("box-km-3").className = null;
     document.getElementById("box-km-4").className = "box-menu-active";
+    document.getElementById("km-content-1").style = "display:none;";
+    document.getElementById("km-content-2").style= "display:none;";
+    document.getElementById("km-content-3").style = "display:none;";
+    document.getElementById("km-content-4").style = "display:block;";
 }
